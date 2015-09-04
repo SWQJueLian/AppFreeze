bed370d066de6f8d96170c28573c5d3124213070
SWQJueLian <1105033640@qq.com>
2015-09-04 15:54:42 +0800

    update v1.05 , better ui layout.

--- gen/com/juelian/appfreeze/R.java
+++ gen/com/juelian/appfreeze/R.java
@@ -20,13 +20,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int all_app=0x7f060000;
         public static final int all_app_listview=0x7f060003;
+        public static final int all_apps=0x7f060001;
         public static final int app_logo=0x7f060004;
         public static final int app_name=0x7f060005;
         public static final int is_freeze=0x7f060006;
-        public static final int textView1=0x7f060002;
-        public static final int users_app=0x7f060001;
+        public static final int textView1=0x7f060000;
+        public static final int users_app=0x7f060002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
