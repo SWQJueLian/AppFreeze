bcee7b7b9796d030b594659c51ce700061034250
SWQJueLian <1105033640@qq.com>
2015-10-11 14:05:49 +0800

    修复 软件冻结器的一键还原冻结所有软件按钮只是显示去除解冻但是实际没有解冻的问题

--- src/com/juelian/appfreeze/MainActivity.java
+++ src/com/juelian/appfreeze/MainActivity.java
@@ -1,11 +1,14 @@
 package com.juelian.appfreeze;
 
+import java.util.Map;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -52,10 +55,23 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				SharedPreferences sharedPreferences = getSharedPreferences("wl", Context.MODE_WORLD_WRITEABLE);
-				Editor editor = sharedPreferences.edit();
-				editor.clear();
-				editor.commit();
+				new Thread(new Runnable() {
+					
+					@Override
+					public void run() {
+						// TODO Auto-generated method stub
+						SharedPreferences sharedPreferences = getSharedPreferences("wl", Context.MODE_WORLD_WRITEABLE);
+						Map<String, ?> map = sharedPreferences.getAll();
+						for (String string : map.keySet()) {
+							Log.e("mijl-->", "packname: "+string);
+							Utils.runCmd("pm unblock " + string);
+							Utils.runCmd("pm enable " + string);
+						}
+						Editor editor = sharedPreferences.edit();
+						editor.clear();
+						editor.commit();
+					}
+				}).start();
 				Toast.makeText(getBaseContext(), R.string.success, Toast.LENGTH_SHORT).show();
 			}
 		});
@@ -69,4 +85,5 @@ public class MainActivity extends Activity {
 		intent.putExtra("filter", filter);
 		this.startActivity(intent);
 	}
+
 }
