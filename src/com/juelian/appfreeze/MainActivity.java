52c50a148838f9c3d5f8ae12a4ca6de2bd3bce31
SWQJueLian <1105033640@qq.com>
2015-10-19 21:14:06 +0800

    冻结列表为空则不指定清空动作

--- src/com/juelian/appfreeze/MainActivity.java
+++ src/com/juelian/appfreeze/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -62,6 +63,12 @@ public class MainActivity extends Activity {
 						// TODO Auto-generated method stub
 						SharedPreferences sharedPreferences = getSharedPreferences("wl", Context.MODE_WORLD_WRITEABLE);
 						Map<String, ?> map = sharedPreferences.getAll();
+						if (map.size()==0) {
+							Looper.prepare();
+							Toast.makeText(getApplicationContext(), "冻结列表为空，无需清空！", 0).show();
+							Looper.loop();
+							return;
+						}
 						for (String string : map.keySet()) {
 							Log.e("mijl-->", "packname: "+string);
 							Utils.runCmd("pm unblock " + string);
@@ -70,9 +77,11 @@ public class MainActivity extends Activity {
 						Editor editor = sharedPreferences.edit();
 						editor.clear();
 						editor.commit();
+						Looper.prepare();
+						Toast.makeText(getBaseContext(), R.string.success, Toast.LENGTH_SHORT).show();
+						Looper.loop();
 					}
 				}).start();
-				Toast.makeText(getBaseContext(), R.string.success, Toast.LENGTH_SHORT).show();
 			}
 		});
 	}
