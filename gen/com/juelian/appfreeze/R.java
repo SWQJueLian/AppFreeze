c10f4b47c2e55a87e4dd01502f29e6ff6f7a5bce
SWQJueLian <1105033640@qq.com>
2015-11-20 15:04:14 +0800

    use mipmap replace drawable

--- gen/com/juelian/appfreeze/R.java
+++ gen/com/juelian/appfreeze/R.java
@@ -16,9 +16,6 @@ public final class R {
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
     }
-    public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-    }
     public static final class id {
         public static final int all_app_listview=0x7f060004;
         public static final int all_apps=0x7f060001;
@@ -33,6 +30,10 @@ public final class R {
         public static final int activity_main=0x7f030000;
         public static final int applist_listview=0x7f030001;
         public static final int applist_listview_item=0x7f030002;
+    }
+    public static final class mipmap {
+        public static final int ic_action_action_https=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class string {
         public static final int add_freeze_message_format=0x7f050004;
